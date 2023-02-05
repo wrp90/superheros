ad2b02223d452c0a9862ed4f5e3db5c836e8c997
Will Papp <wpapp90@gmail.com>
2023-02-05 17:44:01 -0600

    moved div container to global scale to new movie info will replace old, thanks dez

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const moviesContainer = document.getElementById('movies-container');
 const departedButton = document.getElementById('departed-button');
 const guardiansButton = document.getElementById('guardians-button');
+const div = document.createElement('div');
+const img = document.createElement('img');
 
 guardiansButton.addEventListener('click', () => {
     fetch(
         "http://www.omdbapi.com/?i=tt3896198&apikey=1e69acc5"
     ).then(response => response.json()).then(data =>{
         // console.log(data)
-        const div = document.createElement('div');
-        const img = document.createElement('img');
+
         
         div.innerText = data.Title;
         img.src = data.Poster;
     
         moviesContainer.appendChild(div);
-        moviesContainer.appendChild(img);
+        div.appendChild(img);
     })
 })
 
@@ -23,17 +24,13 @@ departedButton.addEventListener('click', () => {
         "http://www.omdbapi.com/?i=tt0407887&apikey=1e69acc5"
     ).then(response => response.json()).then(data =>{
         // console.log(data)
-        const div = document.createElement('div');
-        const img = document.createElement('img');
+
         
         div.innerText = data.Title;
         img.src = data.Poster;
     
         moviesContainer.appendChild(div);
-        moviesContainer.appendChild(img);
+        div.appendChild(img);
     })
 })
-
-
-
 
